5d59eeff938ea247ebc4138950cd6bec39f53f11
Yaswanth <byaswanth2003@gmail.com>
2024-04-16 22:04:12 +0530

    Modify login.js and add FireBase Credientals

--- login.js
+++ login.js
@@ -3,7 +3,13 @@ import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/fir
 import { getFirestore, collection, getDocs, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  // Your Firebase configuration details
+  apiKey: "AIzaSyBGYtobB410Q04qHm7bGSrQBsFdgbvooyY",
+  authDomain: "vaarta-88786.firebaseapp.com",
+  databaseURL: "https://vaarta-88786-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "vaarta-88786",
+  storageBucket: "vaarta-88786.appspot.com",
+  messagingSenderId: "415232456807",
+  appId: "1:415232456807:web:3627768effc74feed73cc2"
 };
 
 const app = initializeApp(firebaseConfig);
